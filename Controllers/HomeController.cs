9ed869071b27722b8928b0bb98c57f4d7b5f812a
Orestis <ozoidis324@gmail.com>
2025-01-11 11:40:02 +0200

    Basic Home Routing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,15 +13,30 @@ namespace ToDoList.Controllers
             _logger = logger;
         }
 
+        #region RoutingDoc
+        //literal -> product
+        //parameter (required) -> {category}
+        //          (optional) -> {category?}
+        //          (default)  -> {category=pro}
+        //          (constrain)-> {category:string}
+        //          (complex constraint) -> {qty:int:max(10)?}
+        //          (catch all)-> {**others}
+        //
+        //Route values are captured by and stored in a dictionary typically used for model binding
+        //Routing only matches URL template segments and doesn't know anything about the data you're expecting those route parameters to contain by default.
+        //You can define a large number of constraints, and its also possible to define more complex ones like regex or min and max.
+        //WARNING Don’t use route constraints to validate general input, such as to check that an email address is valid. Constraints should be used sparingly.
+        //
+        #endregion
+        //home/{today}
+        //home/{tomorrow}
+        [Route("[action]")]
+        [Route("today")]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Program.cs
+++ Program.cs
@@ -16,6 +16,21 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+#region RoutingDoc
+
+//Endpoint selection middleware for given HTTP request.
+//An endpoint in ASP.NET Core is a handler that returns a response. Each endpoint is associated with a URL pattern.
+//The WebApplication stores the registered routes and endpoints in a dictionary making them available to the routing middlewares.
+//Endpoint routing uses a two-step process. The RoutingMiddleware selects which endpoint to execute, and the EndpointMiddleware executes it.
+//If the request URL doesn’t match a route template, the endpoint middleware won’t generate a response.
+
+//Model-Binding
+//The RoutingMiddleware is responsible for matching an incoming request to an endpoint and for extracting the route parameter values, but all the values at that point are strings.
+//It’s only in the EndpointMiddleware that the string values are converted to the real argument types (such as int) needed to execute the endpoint handler.
+
+
+#endregion
+//Adds an EndpointRoutingMiddleware middleware
 app.UseRouting();
 
 app.UseAuthorization();
